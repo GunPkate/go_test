fd5234f4b508f9c9edfd7b47a9fed1e2ee23da49
GunPKate <gun@builk.com>
2022-12-27 11:38:05 +0700

    extract function getpoint

--- rental/customer.go
+++ rental/customer.go
@@ -14,7 +14,7 @@ func NewCustomer(name string) (rcvr Customer) {
 	}
 }
 
-func Charge(r Rental) float64 {
+func (r Rental) Charge() float64 { //one param = one dependency
 	result := 0.0
 	switch r.Movie().PriceCode() {
 	case REGULAR:
@@ -33,6 +33,15 @@ func Charge(r Rental) float64 {
 	return result
 }
 
+func getPoints(r Rental) int {
+	frequentRenterPoints := 0
+	frequentRenterPoints++
+	if r.Movie().PriceCode() == NEW_RELEASE && r.DaysRented() > 1 {
+		frequentRenterPoints++
+	}
+	return frequentRenterPoints
+}
+
 func (rcvr Customer) AddRental(arg Rental) {
 	rcvr.rentals = append(rcvr.rentals, arg)
 }
@@ -43,15 +52,11 @@ func (rcvr Customer) Statement() string {
 	totalAmount := 0.0
 	frequentRenterPoints := 0
 	result := fmt.Sprintf("Rental Record for %v\n", rcvr.Name())
-	for _, each := range rcvr.rentals {
-		thisAmount := Charge(each)
+	for _, r := range rcvr.rentals {
+		frequentRenterPoints += getPoints(r)
 
-		frequentRenterPoints++
-		if each.Movie().PriceCode() == NEW_RELEASE && each.DaysRented() > 1 {
-			frequentRenterPoints++
-		}
-		result += fmt.Sprintf("\t%v\t%.1f\n", each.Movie().Title(), thisAmount)
-		totalAmount += thisAmount
+		result += fmt.Sprintf("\t%v\t%.1f\n", r.Movie().Title(), r.Charge())
+		totalAmount += r.Charge()
 	}
 	result += fmt.Sprintf("Amount owed is %.1f\n", totalAmount)
 	result += fmt.Sprintf("You earned %v frequent renter points", frequentRenterPoints)
